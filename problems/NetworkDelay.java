4ceb4167ae6777c31b28441bbd231391bd326cf5
neelmani07 <manineel25@gmail.com>
2020-06-14 14:47:26 +0530

    14/6

--- problems/NetworkDelay.java
+++ problems/NetworkDelay.java
@@ -1,3 +1,21 @@
+/*
+There are N network nodes, labelled 1 to N.
+
+Given times, a list of travel times as directed edges times[i] = (u, v, w), where u is the source node, v is the target node, and w is the time it takes for a signal to travel from source to target.
+
+Now, we send a signal from a certain node K. How long will it take for all nodes to receive the signal? If it is impossible, return -1.
+
+ 
+
+Example 1:
+diagram missing.
+
+go to :https://leetcode.com/problems/network-delay-time/
+
+Input: times = [[2,1,1],[2,3,1],[3,4,1]], N = 4, K = 2
+Output: 2
+*/
+
 import java.util.*;
 import java.lang.*;
 class NetworkDelay
